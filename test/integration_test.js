bdcb2dd53c9b55dfad61854b6ba2ef12b4c1e2ba
bthesorceror <bthesorceror@gmail.com>
2013-07-29 20:43:19 -0400

    Remove noise from tests

--- test/integration_test.js
+++ test/integration_test.js
@@ -22,29 +22,29 @@ describe('Journeyman', function() {
 
   server.listen();
 
-  server.on('startMiddleware', function(req, res, name) {
-    console.log('********************************');
-    console.log('Middleware: ' + name + ' has started');
-    console.log('********************************');
-  });
+  // server.on('startMiddleware', function(req, res, name) {
+  //   console.log('********************************');
+  //   console.log('Middleware: ' + name + ' has started');
+  //   console.log('********************************');
+  // });
 
-  server.on('endMiddleware', function(req, res, name, time) {
-    console.log('********************************');
-    console.log('Middleware: ' + name + ' completed in ' + time + ' seconds');
-    console.log('********************************');
-  });
+  // server.on('endMiddleware', function(req, res, name, time) {
+  //   console.log('********************************');
+  //   console.log('Middleware: ' + name + ' completed in ' + time + ' seconds');
+  //   console.log('********************************');
+  // });
 
-  server.on('start', function(req, res) {
-    console.log('********************************');
-    console.log('Response started');
-    console.log('********************************');
-  });
+  // server.on('start', function(req, res) {
+  //   console.log('********************************');
+  //   console.log('Response started');
+  //   console.log('********************************');
+  // });
 
-  server.on('end', function(req, res, time) {
-    console.log('********************************');
-    console.log('Response completed in ' + time + ' seconds');
-    console.log('********************************');
-  });
+  // server.on('end', function(req, res, time) {
+  //   console.log('********************************');
+  //   console.log('Response completed in ' + time + ' seconds');
+  //   console.log('********************************');
+  // });
 
   it('works all together', function(done) {
     var zombie = new Zombie();
